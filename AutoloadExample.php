6172a50d865f2d8a8cba431c5e343008f50f8c11
Cristian Tala S. <cristian.tala@gmail.com>
2016-03-08 10:39:29 -0300

    Funcionando. Revisar con datos pre populados.

--- AutoloadExample.php
+++ AutoloadExample.php
@@ -4,6 +4,7 @@ namespace ctala\chartjs;
 
 use ctala\chartjs\ChartJSAsset;
 use \yii\web\View;
+
 /**
  * This is just an example.
  */
@@ -14,7 +15,6 @@ class AutoloadExample extends \yii\base\Widget {
     public $id = "canvas";
     public $height = 450;
     public $width = 650;
-    
     public $labels = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
     public $data = array(
         array(25, 345, 452, 1, 45, 6, 2, 6, 12, 6), array(1, 2, 3, 4, 5, 6, 7, 8, 9, 0),
@@ -45,21 +45,21 @@ class AutoloadExample extends \yii\base\Widget {
         $script = '
             var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
             var barChartData = {
-		labels : ["January","February","March","April","May","June","July"],
+		labels : ' . json_encode($this->labels) . ',
 		datasets : [
 			{
 				fillColor : "rgba(220,220,220,0.5)",
 				strokeColor : "rgba(220,220,220,0.8)",
 				highlightFill: "rgba(220,220,220,0.75)",
 				highlightStroke: "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				data : ' . json_encode($this->data[0]) . '
 			},
 			{
 				fillColor : "rgba(151,187,205,0.5)",
 				strokeColor : "rgba(151,187,205,0.8)",
 				highlightFill : "rgba(151,187,205,0.75)",
 				highlightStroke : "rgba(151,187,205,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				data : ' . json_encode($this->data[1]) . '
 			}
 		]
 	}
@@ -70,11 +70,6 @@ class AutoloadExample extends \yii\base\Widget {
 		});
 	}';
         $this->getView()->registerJs($script, View::POS_END, 'ctala-chartjs-bar');
-    }
-
-    function js_array($array) {
-        $temp = array_map('js_str', $array);
-        return '[' . implode(',', $temp) . ']';
     }
 
 }
